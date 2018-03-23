5b0edc461ec448b5b9d36716279bcab2bf8a06d3
Cooper Knaak <cooperknaak@gmail.com>
2018-03-23 12:44:16 -0500

    Implemented valid Power test (one is broken).

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -11,7 +11,7 @@ namespace TravisCILab
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
-            Assert.AreEqual(12, Program.Add("5", "7"));
+            Assert.AreEqual(13, Program.Add("5", "7"));
         }
 
         [Test]
@@ -28,6 +28,14 @@ namespace TravisCILab
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+
+        [Test]
+        public void Power_Valid()
+        {
+            Assert.AreEqual(100, Program.Power("10", "2"));
+            Assert.AreEqual(81, Program.Power("3", "4"));
+            Assert.AreEqual(65, Program.Power("4", "3"));
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
